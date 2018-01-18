0468711c60e7e6b2f08614473274a52d47d09592
zzxfighting <1466454216@qq.com>
2018-01-18 11:05:41 +0800

    2

--- src/main/java/com.chatRobot/service/LoginService.java
+++ src/main/java/com.chatRobot/service/LoginService.java
@@ -1,10 +1,7 @@
 package com.chatRobot.service;
-
 import com.chatRobot.model.Sales;
 import org.springframework.web.servlet.ModelAndView;
-
 import java.io.UnsupportedEncodingException;
-
 /**
  * Created by Cookbooks on 2018/1/12.
  */
@@ -12,5 +9,4 @@ public interface LoginService {
     //判断是否存在该用户
      boolean isSales(Sales sales);
      void Login(ModelAndView res, String result) throws UnsupportedEncodingException;
-
 }
